faab7411635daa4e08767d303942a6623a445ae7
wyhwong <wyhwong@link.cuhk.edu.hk>
2022-12-29 02:12:47 +0800

    Added HKT timestamp and initialization of plots

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:20.04
+ENV TZ=Asia/Hong_Kong
 ARG DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get update && apt-get install -y python3 python3-pip -y
-RUN pip3 install jupyterthemes notebook
+RUN apt-get update && apt-get install -y tzdata python3 python3-pip -y
+RUN pip3 install seaborn numpy pandas matplotlib jupyterthemes notebook
 RUN jt -t monokai
---
+++ utils/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def get_base_plot(nrows=1, ncols=1, height=6, width=10, title="", ylabel="", xlabel="", tpad=2.5, lpad=0.1, bpad=0.12, fontsize=12):
+    fig, axes = plt.subplots(nrows, ncols, figsize=(width, height))
+    fig.tight_layout(pad=tpad)
+    fig.subplots_adjust(left=lpad, bottom=bpad)
+    fig.suptitle(title, fontsize=fontsize)
+    fig.text(x=0.04, y=0.5, s=ylabel, fontsize=fontsize, rotation="vertical", verticalalignment='center')
+    fig.text(x=0.5, y=0.04, s=xlabel, fontsize=fontsize, horizontalalignment='center')
+    return fig, axes
